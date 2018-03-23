938a38bc800e431c148cce5da1143ed6b0a2758e
York13 <york13@inbox.ru>
2018-03-23 20:39:50 +0400

    Небольшие переделки в Main.

--- TestJava14Maven/src/main/java/Main.java
+++ TestJava14Maven/src/main/java/Main.java
@@ -1,13 +1,9 @@
-import Service.SummaryService;
 import Service.SummaryServiceImpl;
 
-/*
-*   За предоставленный рабочий вариант программы огромное спасибо Стратонову Алексею из SimbirSoft
-*/
 public class Main {
 
     public static void main(String[] args) {
-        SummaryService summaryService = new SummaryServiceImpl("person.properties");
+        SummaryServiceImpl summaryService = new SummaryServiceImpl("person.properties");
         summaryService.createHtmlFile("src/main/java/Webapp/summary.html");
     }
 }
